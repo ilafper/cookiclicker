446b7d800db732f0bf531430f5802e33e084b8f2
ilafper <ivanaalfaya@gmail.com>
2026-01-16 18:23:02 +0100

    modaal hecho para el usuarios mas temporizador, falta ranking y estilo

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, NgZone  } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
@@ -22,24 +22,42 @@ export class HomeComponent {
   total_cliks: number = 0;
   lista_usuarios: any[] = [];
 
-  constructor(private cdr: ChangeDetectorRef) {}
+  tiempo: number = 5;
+
+  intervalo: any;
+
+  modal: boolean = false;
+  modal_cero_cliks:boolean=false;
+
+  mensajePerso: string="No has pulsado ninguna vez";
+  constructor(private cdr: ChangeDetectorRef, private ngZone: NgZone) { }
 
   ngOnInit() {
     this.loadUsers();
   }
 
   sumar() {
-    this.total_cliks += 1;
+    //usar ngzone para que angular detecte los cambios relaizados por que hay veces cuando haces clicks muy rapido no los detecta.
+    this.ngZone.run(() => {
+      this.total_cliks += 1;
+      this.cdr.detectChanges(); // Forzar detección inmediata
+      console.log('Click registrado:', this.total_cliks);
+    });
   }
 
-  
+
+
+
+
+
+
 
   async loadUsers() {
     try {
-      const usuarios = await window.electron.invoke('get-users');     
+      const usuarios = await window.electron.invoke('get-users');
       this.lista_usuarios = usuarios;
       //si detecta un cambio actualiza la vista
-      this.cdr.detectChanges()
+      this.cdr.detectChanges();
 
       console.log('Usuarios cargados:', this.lista_usuarios);
 
@@ -49,5 +67,76 @@ export class HomeComponent {
     }
   }
 
+  iniciarTiempo() {
+    if (this.intervalo) {
+      clearInterval(this.intervalo);
+    }
+    //cuando vuelvas a pulsar el boton se pone de nuevo a el valor
+    this.tiempo = 5;
+    this.intervalo = setInterval(() => {
+      console.log("Tiempo :", this.tiempo);
+      this.tiempo--;
+      this.cdr.detectChanges();
+
+      if (this.tiempo === 0) {
+        console.log("se acabo el tiempo");
+        console.log("mostrando modal");
+        clearInterval(this.intervalo);
+        this.intervalo = null;
+        this.cdr.detectChanges();
+
+        if (this.total_cliks===0) {
+          this.mostrarModal();
+        }else if(this.total_cliks>0){
+          this.mostrarModal2();
+        }
+       
+
+      }
+    }, 1000); // va de 1 en uno 1
+
+  }
+
+
+  mostrarModal() {
+    this.modal = true;
+    document.body.style.overflow = 'hidden';
+    this.cdr.detectChanges();
+  }
+
+  mostrarModal2() {
+    this.modal_cero_cliks = true;
+    document.body.style.overflow = 'hidden';
+    this.cdr.detectChanges();
+  }
   
+  cerrarModal() {
+    this.modal = false;
+    document.body.style.overflow = 'auto';
+    this.cdr.detectChanges();
+    this.reiniciar();
+
+  }
+
+  reiniciar(){
+    this.total_cliks=0;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
